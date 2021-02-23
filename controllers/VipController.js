adb00e44690c6f1b3796ab9baac2388a59a5d51f
Florian Vivet <florian.vivet@etu.unilim.fr>
2021-02-23 09:59:19 +0100

    début de requetes

--- controllers/VipController.js
+++ controllers/VipController.js
@@ -7,33 +7,46 @@ var async = require('async');
 
 module.exports.Repertoire = 	function(request, response){
 
-   console.log("Lettre = " + request.params.letter);
-   response.title = 'Répertoire des stars';
+  console.log("Lettre = " + request.params.letter);
+  response.title = 'Répertoire des stars';
 
-   async.parallel ([
+  async.parallel ([
 
-     function (callback){
-         model.firstLetterVip(function(err, result)  {callback(null,result) } )
-     },
-     function (callback){
-        model.vipByLetter(request.params.letter, (function(errE, resE)  {callback(null,resE) } ));
-     },
-   ],
-   function (err, result){
-      if (err) {
-          console.log(err);
-          return;
-      }
-      response.lettre = request.params.letter;
-      response.firstLetters = result[0];
-      response.noms = result[1];
-      response.render('repertoireVips', response);
+    function (callback){
+      model.firstLetterVip(function(err, result)  {callback(null,result) } )
+    },
+    function (callback){
+      model.vipByLetter(request.params.letter, (function(errE, resE)  {callback(null,resE) } ));
+    },
+  ],
+  function (err, result){
+    if (err) {
+      console.log(err);
+      return;
     }
-  );
+    response.lettre = request.params.letter;
+    response.firstLetters = result[0];
+    response.noms = result[1];
+    response.render('repertoireVips', response);
+  }
+);
 }
 
+
+
+
+
 module.exports.ficheVip = 	function(request, response){
+ response.title = 'Test à supprimer';
 
-      response.render('ficheVip', response);
+ model.vipByNum(request.params.numero, (function(err, result){  // appel le module test qui exécute la requete SQL
+     if (err) {
+         console.log(err);
+         return;
+     }
+    response.vip=result;
+    console.log(response.vip);
 
+    response.render('ficheVip', response); // appel la vue Handlebars qui va afficher le résultat
+} ));
 }
